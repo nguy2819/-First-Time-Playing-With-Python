5ae67e814a15fa373690cdd6a78f642b32dae1cf
Tien Borland <nguy2819@crk.umn.edu>
2018-10-03 16:49:47 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+# Write out "Hello World"
+print("Hello World")
+
+# Drawing a Shape
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+# Variables & Data Types
+
+character_name = "George"
+character_age = "70"
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+# Also we can change in the character_name from this second part down
+character_name = "Mike"
+print("He is really like the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
+
+# Result will be:
+# There once was a man named George,
+# he was 70 years old.
+# He is really like the name George,
+# but didn't like being 70.
